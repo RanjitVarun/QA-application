01917d98b17fdbdf587b16838e6212e95b0760a3
RanjitVarun <ranjit.gaudaman@aspiresys.com>
2020-07-16 20:03:05 +0530

    made changes in Usermodels fields

--- user/models.py
+++ user/models.py
@@ -3,16 +3,16 @@ from django.db import models
 class UserDetails(models.Model): 
     first_name=models.CharField(max_length=20) 
     last_name=models.CharField(max_length=20) 
-    DOB=models.CharField(max_length=15) 
+    DOB=models.DateField()
     gender=models.CharField(max_length=10)
-    password=models.CharField(max_length=10) 
+    password=models.CharField(max_length=20) 
   
     def __str__(self): 
       return self.first_name 
 
 class Email(models.Model):
      user_id=models.ForeignKey(UserDetails, on_delete=models.CASCADE,related_name="email" ) 
-     email=models.EmailField(max_length=254) 
+     email=models.EmailField(max_length=20) 
      
      def __str__(self): 
        return self.email    
@@ -26,15 +26,15 @@ class Mobile(models.Model):
 
 class ResAddress(models.Model):
      user_id=models.ForeignKey(UserDetails, on_delete=models.CASCADE,related_name="resaddress") 
-     line1=models.CharField(max_length=11) 
-     line2=models.CharField(max_length=11)
-     line3=models.CharField(max_length=11)
-     landmark=models.CharField(max_length=11)
-     state=models.CharField(max_length=11)
+     line1=models.CharField(max_length=20) 
+     line2=models.CharField(max_length=20)
+     line3=models.CharField(max_length=20)
+     landmark=models.CharField(max_length=20)
+     state=models.CharField(max_length=20)
      pin=models.IntegerField()
 
      def __str__(self): 
-       return "Residential Address Added"          
+       return '%s %s' % (self.line1, self.line2)         
 
 class OfficeAddress(models.Model):
      user_id=models.ForeignKey(UserDetails, on_delete=models.CASCADE,related_name="offaddress") 
@@ -46,23 +46,23 @@ class OfficeAddress(models.Model):
      pin=models.IntegerField()
 
      def __str__(self): 
-       return "Office Address Added"
+       return '%s %s' % (self.line1, self.line2) 
 
 class Education(models.Model):
-    degree=models.CharField(max_length=10)
+    degree=models.CharField(max_length=20)
 
     def __str__(self): 
        return self.degree  
 
 class Course(models.Model):
     degree=models.ForeignKey(Education, on_delete=models.CASCADE,related_name="degree_course") 
-    course=models.CharField(max_length=50)
+    course=models.CharField(max_length=20)
 
     def __str__(self): 
        return self.course 
 
 class Board(models.Model):
-    board=models.CharField(max_length=50)
+    board=models.CharField(max_length=20)
 
     def __str__(self): 
        return self.board             
@@ -74,10 +74,10 @@ class EducationRelUser(models.Model):
     board=models.ForeignKey(Board, on_delete=models.CASCADE,related_name="board_relation")
 
     def __str__(self): 
-       return "Educational details Updated"  
+       return '%s %s' % (self.degree, self.course)  
 
 class Skillset(models.Model):
-    mainskill=models.CharField(max_length=50)
+    mainskill=models.CharField(max_length=20)
     subskill=models.IntegerField()
     
     def __str__(self): 
@@ -88,4 +88,4 @@ class skillsetRel(models.Model):
     user_id=models.ForeignKey(UserDetails, on_delete=models.CASCADE,related_name="user_skill")
     
     def __str__(self): 
-       return "skill Relation with updated"        
+       return '%s %s' % (self.skill_id, self.user_id)        
